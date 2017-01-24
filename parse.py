b91e9b669483406e9641d5907c549be2116fa926
divyaPrakash <divs4debu@gmail.com>
2017-01-24 19:02:04 +0530

    added complete directory parse

--- parse.py
+++ parse.py
@@ -1,8 +1,21 @@
 import xmltodict
+from os import listdir
+import json
 
-project_path = "/home/divy/Work/ProjectWork/Roboclub/ProjectDetails"
-project_list = ["/AutoBuggy"]
-
-project_details = open(project_path+project_list[0]+"/details.xml").read()
-project_dict = xmltodict.parse(project_details)
-print(project_dict['project']['thumb'])
+project_path = "/home/divy/Work/ProjectWork/Roboclub/ProjectDetails" 
+project_list  = list()
+[project_list.append("/{}".format(dirnames)) for dirnames in listdir(project_path)]
+#print(project_list)
+for project in project_list:
+    try:
+        project_details = open(project_path+project+"/details.xml").read()
+    except FileNotFoundError:
+        pass
+    project_dict = xmltodict.parse(project_details)
+    try:
+        json_file = open("json.txt","a")
+        json_file.write(json.dumps(project_dict,indent=5))
+    except KeyError:
+        pass
+    
+        
